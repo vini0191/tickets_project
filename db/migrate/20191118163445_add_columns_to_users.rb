6f3435b1a5588d19c4b080ac31fb663835f4ea09
Vinicius Alvim <vini0191@gmail.com>
2019-11-18 13:40:40 -0300

    Basic setup

---
+++ db/migrate/20191118163445_add_columns_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :city, :string
+    add_column :users, :photo, :string
+  end
+end
