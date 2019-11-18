1da5071b2a2211f0cc76f79ae6cf5d2731eb8da5
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2019-11-18 16:53:13 -0300

    event index and show, done

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,13 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: %i[show]
+
+  def index
+    @events = Event.all
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
 end
