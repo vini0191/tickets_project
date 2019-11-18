537114ce882de905c0f134219db23d83f3090703
Vinicius Alvim <vini0191@gmail.com>
2019-11-18 18:14:54 -0300

    Ongoing...

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,44 @@
 class TicketsController < ApplicationController
+  before_action :set_ticket, only: %i[show edit update destroy]
+
+  def index
+    @tickets = Ticket.all
+  end
+
+  def show
+  end
+
+  def new
+    @ticket = Ticket.new
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+    @ticket.save
+    raise
+    redirect_to tickets_path
+  end
+
+  def edit
+  end
+
+  def update
+    @ticket.update(ticket_params)
+    redirect_to tickets_path
+  end
+
+  def destroy
+    @ticket.destroy
+    redirect_to tickets_path
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:price, :seat, :type)
+  end
+
+  def set_ticket
+    @ticket = Ticket.find(params[:id])
+  end
 end
