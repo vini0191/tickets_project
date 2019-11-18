147af2a2091a16300b9bf133a4c35fbb9d121c1a
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2019-11-18 17:24:53 -0300

    event destroy, done

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,13 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: %i[show]
+  before_action :set_event, only: %i[show destroy]
 
   def index
     @events = Event.all
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
   root to: 'pages#home'
 
-  resources :events, only: %i[index show] do
+  resources :events, only: %i[index show destroy] do
     resources :tickets, only: %i[show]
   end
 
