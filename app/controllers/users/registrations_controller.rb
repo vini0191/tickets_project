d4030d3151e8c2f945ba87799a4900fcaea346df
Juca Esmanhoto <jucaesmanhoto@gmail.com>
2019-11-21 11:56:00 -0300

    added descriptions

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -47,7 +47,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[attribute description])
   end
 
   # The path used after sign up.
